52ad19e7d027facfca738965123d1a9a15f087be
Jamest011 <97201192+Jamest011@users.noreply.github.com>
2022-03-09 21:41:06 -0600

    Edited game over message

--- cycles/game/scripting/handle_collisions_action.py
+++ cycles/game/scripting/handle_collisions_action.py
@@ -78,7 +78,7 @@ class HandleCollisionsAction(Action):
             position = Point(x, y)
 
             message = Actor()
-            message.set_text("Game Over! The winner is !")
+            message.set_text("Game Over!")
             message.set_position(position)
             cast.add_actor("messages", message)
 
